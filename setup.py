b127977019a0185854c71b10b486a6221bcf758a
Bobby <karma0@gmail.com>
2018-10-11 13:56:43 -0600

    Removing 3.7 from supported Python versions

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
     description="A Python implementation of the choreo robotics framework",
     entry_points={
